c1c52d54310dae5c6198bc2af03b3766137c3572
Gianmarco <53410676+GianmarcoFolchi@users.noreply.github.com>
2023-10-24 16:43:20 -0400

    Time: 65 ms (65.56%), Space: 17.6 MB (22.11%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,33 +1,9 @@
-class Solution(object):
-    def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
-        dic = dict()
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        indexMap = {}
+        for i, num in enumerate(nums): 
+            indexMap[num] = i
         
-        for i, num in enumerate(nums):
-            dic.setdefault(num, []).append(i)
-            
-        for num in nums:
-            currTarget = target - num
-            if currTarget in dic:
-                if currTarget == num:
-                    if len(dic[num]) > 1:
-                        return [dic[num][0], dic[num][1]]
-                    else:
-                        continue
-                        
-                return [dic[num][0], dic[currTarget][0]]
-                    
-                    
-        
-        
-
-            
-                
-        
-        
-                
-                
+        for i, num in enumerate(nums): 
+            if target - num in indexMap and indexMap[target - num] != i:
+                return [i, indexMap[target - num]]
